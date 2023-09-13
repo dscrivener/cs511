782ce24a9e5b51d9d79c99a494d6d0f37f2cdfc2
Daniel Scrivener <dscriv@bu.edu>
2023-09-13 13:22:37 -0400

    finish 3c, hw1

--- Math2001/my_scripts/hw1/3c.lean
+++ Math2001/my_scripts/hw1/3c.lean
@@ -12,4 +12,6 @@ example {p q r : Prop} (h1 : p ∧ ¬q → r) (h2 : ¬r) (h3 : p) : q := by
     have h_pnq : p ∧ ¬q := And.intro h3 h_nq
     have h_r : r := by apply h1 h_pnq
     contradiction
-  apply h_contra
+  have h_nnq : ¬¬q := by apply h_contra
+  apply notnotE h_nnq
+  
