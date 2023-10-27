a1722d3732bfac8894814f88ddf473b5a0c0f3f1
Daniel Scrivener <dscriv@bu.edu>
2023-10-27 16:01:56 -0400

    hw8: finished book exercises

--- Math2001/my_scripts/hw8/4c.lean
+++ Math2001/my_scripts/hw8/4c.lean
@@ -10,4 +10,13 @@ import Library.Tactic.Use
 -- 6.1.7, Exercise 2
 
 example {a : ℝ} (ha : -1 ≤ a) (n : ℕ) : (1 + a) ^ n ≥ 1 + n * a := by
-  sorry
+  simple_induction n with k IH
+  · ring
+    numbers
+  · have h_2 : 0 ≤ 1 + a := by addarith [ha]
+    calc
+      (1 + a) ^ (k + 1) = (1 + a) ^ k * (1 + a) := by ring
+      _ ≥ (1 + k * a) * (1 + a) := by rel [IH]
+      _ = k * a ^ 2 + k * a + a + 1 := by ring
+      _ ≥ k * a + a + 1 := by extra
+      _ = 1 + (k + 1) * a := by ring
--- Math2001/my_scripts/hw8/4d.lean
+++ Math2001/my_scripts/hw8/4d.lean
@@ -7,8 +7,19 @@ import Library.Tactic.Numbers
 import Library.Tactic.Addarith
 import Library.Tactic.Use
 
+notation3 (prettyPrint := false) "forall_sufficiently_large "(...)", "r:(scoped P => ∃ C, ∀ x ≥ C, P x) => r
+
 -- 6.1.7, Exercise 6
 
 example : forall_sufficiently_large n : ℕ, (3:ℤ) ^ n ≥ 2 ^ n + 100 := by
   dsimp
-  sorry
+  use 5
+  intro x
+  intro h_x_ge_5
+  induction_from_starting_point x, h_x_ge_5 with k hk IH
+  · numbers
+  · calc
+      (3:ℤ) ^ (k + 1) = 2 * 3 ^ k + 3 ^ k := by ring
+      _ ≥ 2 * (2 ^ k + 100) + 3 ^ k := by rel [IH]
+      _ = 2 ^ (k + 1) + 100 + (100 + 3 ^ k) := by ring
+      _ ≥ 2 ^ (k + 1) + 100 := by extra
